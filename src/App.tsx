a08ef3875beb292e45515a6a4297d8399909fa40
ouxnq <buenop.vinicius@gmail.com>
2024-08-16 11:37:04 -0300

    feat: implemented crud on categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
+import ListaCategorias from "./components/Categorias/listaCategorias/ListaCategorias";
+import FormularioCategorias from "./components/Categorias/formularioCategorias/FormularioCategorias";
+import DeletarCategoria from "./components/Categorias/deletarCategoria/DeletarCategoria";
 
 function App() {
   return (
     <>
-      <NavBar />
       <BrowserRouter>
+        <NavBar />
         <div className='min-h-[70vh]'>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormularioCategorias />} />
+            <Route path="/editarCategoria/:id" element={<FormularioCategorias />} />
+            <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom"
 
 
 function NavBar() {
     return (
         <div className='w-full bg-[#3a405a] text-white flex justify-center py-4'>
             <div className="container flex justify-between text-lg">
-                <h2 className="text-2xl font-semibold">Farmácia Precin</h2>
+                <Link to='/home' className="text-2xl font-semibold">Farmácia Precin</Link>
                 <div className='flex gap-4'>
                     <p>Produtos</p>
-                    <p>Categorias</p>
-                    <p>Cadastrar Categoria</p>
+                    <Link to='/categorias' className='hover:underline'>Categorias</Link>
+                    <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
                 </div>
             </div>
         </div>
